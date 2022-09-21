9e2d605779edf7c0c4c9dee97827fcddb8c65b69
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-09-21 10:50:35 -0300

    update mesas

--- controllers/mesasController.js
+++ controllers/mesasController.js
@@ -31,6 +31,24 @@ module.exports = {
         } catch (error) { 
             return response.status(500).json({confirma: 'Erro', message: error});
         }   
-    },
+    }, 
+    async update(request, response) { 
+        try {
+                // parâmtros passados via corpo da requisição
+            const { mes_nome, mes_status, mes_lugares, ped_id } = request.body;
+                // parâmetro passado via url na chamada da api pelo front-end
+            const { mes_id } = request.params; 
+                // instrução sql para atualização
+            const sql = 'UPDATE mesas SET mes_nome = ?, mes_status = ?, mes_lugares = ?, ped_id = ? WHERE mes_id = ?;';  
+                // definição de array com os parâmetros que receberam os valores do front-end
+            const values = [mes_nome, mes_status, mes_lugares, ped_id, mes_id];   
+                // executa a instrução de atualização no banco de dados    
+            const atualizacao = await db.query(sql, values);
+                // Mensagem de retorno no formato JSON
+            return response.status(200).json({confirma: 'Sucesso', message: 'Dados atualizados'});            
+        } catch (error) { 
+            return response.status(500).json({confirma: 'Erro', message: error});
+        }        
+    }, 
 };
 
