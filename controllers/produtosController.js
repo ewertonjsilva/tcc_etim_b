1a1bc8cecdb8ef19e12d26270dd92dc18918797b
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-10-05 10:02:37 -0300

    lista produtos basico

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -4,7 +4,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarProdutos(request, response) { 
         try {
-            return response.status(200).json({confirma: 'Produtos'});   
+            const sql = 'SELECT prd_id, prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao FROM produtos;';  
+            const produtos = await db.query(sql); 
+            return response.status(200).json({confirma: 'Sucesso', nResults: produtos[0].length, message: produtos[0]});   
         } catch (error) { 
             return response.status(500).json({confirma: 'Erro', message: error});
         }
